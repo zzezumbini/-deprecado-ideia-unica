6f7ecae5ed44e7cabafbfdc0c957b81de599d007
zzezumbini <zzezumbini@gmail.com>
2023-04-28 11:34:34 -0300

    inclusão de navbar

---
+++ components/GrayScaleNavbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: theme.palette.grey[900],
+    [theme.breakpoints.up('md')]: {
+      backgroundColor: theme.palette.grey[800],
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  title: {
+    color: theme.palette.grey[50],
+  },
+  button: {
+    color: theme.palette.grey[50],
+    marginLeft: theme.spacing(2),
+    [theme.breakpoints.up('md')]: {
+      marginLeft: theme.spacing(4),
+    },
+  },
+}));
+
+const GrayScaleNavbar = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static" className={classes.appBar}>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h6" className={classes.title}>
+          Zé Zumbini
+        </Typography>
+        <div>
+          <Button className={classes.button}>Home</Button>
+          <Button className={classes.button}>About</Button>
+          <Button className={classes.button}>Contact</Button>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default GrayScaleNavbar;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import React from "react";
+import GrayScaleNavbar from "../components/GrayScaleNavbar";
+
 function Home() {
-    return <div>Home</div>
+    return (
+        <div>
+            <GrayScaleNavbar />
+        </div>
+    );
 }
 
 // novo padrão de módulos do javascript
